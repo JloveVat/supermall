3d3653a20c24f46a7177bc89066d7c3e21e10378
JloveVat <1473357280@qq.com>
2021-07-05 23:12:08 +0800

    首页详情页

---
+++ src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper class="detail-swiper">
+    <swiper-item v-for="(item, index) in toImages" :key="index">
+        <img :src="item" alt=""/>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from 'components/common/swiper'
+
+export default {
+  props: {
+    toImages: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  },
+}
+</script>
+
+<style>
+  .detail-swiper {
+    height: 250px;
+    overflow: hidden;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,46 +6,67 @@
         购物街
       </div>
     </nav-bar>
-    <!-- 轮播图 -->
-    <home-swiper :banners="banners"/>
-    <!-- 推荐 -->
-    <recommend-view :recommends="recommends"></recommend-view>
-    <!-- 本周流行 -->
-    <feature-view/>
-    <!-- 导航 -->
-    <tab-control class="tab-control" :titles='["流行", "新款", "精选"]'></tab-control>
-    <!-- 商品列表 -->
-    <goods-list :goods="goods['pop'].list"/>
-    <ul>
-      <li>内容</li>
-      <li>内容</li>
-      <li>内容</li>
-      <li>1</li>
-    </ul>
+    <!-- 准备吸顶的导航 -->
+    <tab-control 
+          :titles='["流行", "新款", "精选"]'
+          @tabClick="tabClick"
+          ref="tabControl1"
+          class="tab-control"
+          v-show="isTabFixed"/>
+    <!-- 滚动 -->
+    <scroll class="content" 
+            ref="scroll" 
+            :probe-type="3" 
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <!-- 轮播图 -->
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
+      <!-- 推荐 -->
+      <recommend-view :recommends="recommends"></recommend-view>
+      <!-- 本周流行 -->
+      <feature-view/>
+      <!-- 中间导航 -->
+      <tab-control 
+          :titles='["流行", "新款", "精选"]'
+          @tabClick="tabClick"
+          ref="tabControl2"/>
+      <!-- 商品列表 -->
+      <goods-list :goods="showGoods"/>
+    </scroll>
+      <back-top @click.native="backClick" v-show="isShowBackTop"/>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import Scroll from 'components/common/scroll/Scroll'
+
 import TabControl from 'components/content/tabControl/TabControl'
 import GoodsList from 'components/content/goods/GoodsList'
+import BackTop from 'components/content/backTop/BackTop'
 
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
 import FeatureView from './childComps/FeatureView'
 
 import { getHomeMultidata, getHomeGoods } from 'network/home'
+import {itemListenerMixin} from 'common/mixin'
 
 export default {
   components: {
     NavBar,
+    Scroll,
+
     TabControl,
     GoodsList,
+    BackTop,
 
     HomeSwiper,
     RecommendView,
     FeatureView
   },
+  mixins: [itemListenerMixin],
   data() {
     return {
       banners: [],
@@ -54,9 +75,32 @@ export default {
         'pop': {page: 0, list: []},
         'new': {page: 0, list: []},
         'sell': {page: 0, list: []}
-      }
+      },
+      currentType: 'pop',
+      isShowBackTop: false,
+      tabOffsetTop: 0,
+      isTabFixed: false,
+      saveY: 0,
     }
   },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list
+    }
+  },
+  activated() {
+    // console.log('activated');
+    this.$refs.scroll.scrollTo(0, this.saveY, 0)
+    this.$refs.scroll.refresh()
+  },
+  deactivated() {
+    // console.log('deactivated');
+    this.saveY = this.$refs.scroll.getScrollY()
+
+    // 2. 取消全局事件的监听
+    this.$bus.$off('itemImageLoad', this.itemImgListener)
+  },
+
   created() {
     // 1. 请求多个数据
     this.getHomeMultidata()
@@ -65,8 +109,11 @@ export default {
     this.getHomeGoods('pop')
     this.getHomeGoods('new')
     this.getHomeGoods('sell')
+
+    
   },
   methods: {
+    // 网络请相关方法
     // 1. 请求多个数据
     getHomeMultidata() {
       getHomeMultidata().then(res => {
@@ -80,31 +127,94 @@ export default {
       const page = this.goods[type].page + 1
       getHomeGoods(type, page).then(res => {
         this.goods[type].list.push(...res.data.list)
-        this.goods[type].page++
+        this.goods[type].page += 1
+
+        this.$refs.scroll.finishPullUp()
       })
-    }
+    },
+
+    // 轮播图加载完成
+    swiperImageLoad() {
+      // 获取tabControl的offsetTop
+      this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop
+      // console.log(this.tabOffsetTop);
+    },
+    // 事件监听方法
+    tabClick(index) {
+      // console.log(index);
+      switch(index) {
+        case 0:
+          this.currentType = 'pop'
+          break;
+        case 1:
+          this.currentType = 'new'
+          break;
+        case 2:
+          this.currentType = 'sell'
+          break;
+      }
+      this.$refs.tabControl1.currentIndex = index
+      this.$refs.tabControl2.currentIndex = index
+    },
+    // 内容滚动
+    contentScroll(position) {
+      // 1. 判断BackTop是否显示
+      // console.log(position);
+      this.isShowBackTop = (-position.y) > 800
+      // 2. 决定tabControl是否吸顶
+      this.isTabFixed = (-position.y) > this.tabOffsetTop
+    },
+    // 上拉加载更多
+    loadMore() {
+      // console.log('加载更多');
+      this.getHomeGoods(this.currentType)
+    },
+    // 返回顶部
+    backClick() {
+      // console.log('返回顶部');
+      this.$refs.scroll.scrollTo(0, 0)
+    },
 
   }
 }
 </script>
 
-<style>
+<style scoped>
   #home {
-    padding-top: 44px;
+    /* padding-top: 44px; */
+    height: 100vh;
+    position: relative;
   }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
 
-    position: fixed;
+    /* position: fixed;
     top: 0;
     right: 0;
     left: 0;
+    z-index: 9; */
+  }
+  /* 吸顶的导航条 */
+  .tab-control {
+    position: relative;
     z-index: 9;
   }
 
-  .tab-control {
-    position:sticky;
+  .content {
+    height: calc(100% - 93px);
+    overflow: hidden;
+
+    position: absolute;
     top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
+  /* 第二种方式 */
+  /* .content {
+    height: calc(100% - 93px);
+    overflow: hidden;
+    margin-top: 44px;
+  } */
 </style>
